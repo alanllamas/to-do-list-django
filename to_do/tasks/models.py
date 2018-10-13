b4a35ab2d2b280176cb5f2cefa24315a8c24e736
alanllamas <alan@alans-MacBook-Pro.local>
2018-10-13 16:05:20 -0500

    added authentication to api

--- to_do/tasks/models.py
+++ to_do/tasks/models.py
@@ -4,21 +4,21 @@ from to_do import settings
 
 
 class Task(models.Model):
+    PRIORITY_CHOICES     = (
+    ('4', 'Emergency'),
+    ('3', 'High'),
+    ('2', 'Medium'),
+    ('1', 'Low'),
+    )
+    priority             = models.CharField(
+    max_length  = 1,
+    choices     = PRIORITY_CHOICES,
+    default     = '2',
+    )
+    user                 = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='tasks', on_delete=models.CASCADE, null=False)
     name                 = models.CharField(max_length=100, unique=True, blank=False, null=False)
     description          = models.TextField(max_length=300, blank=True)
     duration             = models.DecimalField(max_digits=3, decimal_places=0, blank=False, null=False)
-    user                 = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='tasks', on_delete=models.CASCADE, null=False)
-    PRIORITY_CHOICES     = (
-        ('4', 'Emergency'),
-        ('3', 'High'),
-        ('2', 'Medium'),
-        ('1', 'Low'),
-    )
-    priority             = models.CharField(
-        max_length  = 1,
-        choices     = PRIORITY_CHOICES,
-        default     = '2',
-    )
     dead_line            = models.DateField(auto_now=False, auto_now_add=False, blank=False)
     done                 = models.BooleanField(blank=False,default=False)
     time_done            = models.DecimalField(decimal_places=0, max_digits=3, blank=True, null=True)
--- to_do/users/views.py
+++ to_do/users/views.py
@@ -1,18 +1,21 @@
 from users.models import User
 from rest_framework import generics
+from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
- 
+from signal import *
 from users.serializers import UserSerializer
- 
- 
+
+
 class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
- 
- 
+    permission_classes = [permissions.IsAuthenticated]
+
+
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = UserSerializer
- 
+    permission_classes = [permissions.IsAuthenticated]
+
     def get_queryset(self):
         return User.objects.all().filter(username=self.request.user)
